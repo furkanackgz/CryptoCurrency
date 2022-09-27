8a482e3496dced6b507bfa7400276691c13a511d
furkanackgz <furkanackgz99@gmail.com>
2022-09-27 17:50:32 +0300

    Add details view layer ui elements and assign it to view layer

--- CryptoCurrency/Details Module/View/DetailsView.swift
+++ CryptoCurrency/Details Module/View/DetailsView.swift
@@ -9,6 +9,34 @@ import UIKit
 
 class DetailsView: UIViewController {
 
+    // MARK: - Features
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var currencyImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var symbolLabel: UILabel!
+    @IBOutlet weak var chartView: UIView!
+    @IBOutlet weak var currentPriceValueLabel: UILabel!
+    @IBOutlet weak var marketCapValueLabel: UILabel!
+    @IBOutlet weak var marketCapRankValueLabel: UILabel!
+    @IBOutlet weak var fullyDilutedValuationValueLabel: UILabel!
+    @IBOutlet weak var totalVolumeValueLabel: UILabel!
+    @IBOutlet weak var high24hValueLabel: UILabel!
+    @IBOutlet weak var low24hValueLabel: UILabel!
+    @IBOutlet weak var priceChange24hValueLabel: UILabel!
+    @IBOutlet weak var priceChangePercentage24hValueLabel: UILabel!
+    @IBOutlet weak var marketCapChange24hValueLabel: UILabel!
+    @IBOutlet weak var marketCapChangePercentage24hValueLabel: UILabel!
+    @IBOutlet weak var circulatingSupplyValueLabel: UILabel!
+    @IBOutlet weak var totalSupplyValueLabel: UILabel!
+    @IBOutlet weak var maxSupplyValueLabel: UILabel!
+    @IBOutlet weak var athValueLabel: UILabel!
+    @IBOutlet weak var athChangePercentageValueLabel: UILabel!
+    @IBOutlet weak var athDateValueLabel: UILabel!
+    @IBOutlet weak var atlValueLabel: UILabel!
+    @IBOutlet weak var atlChangePercentageValueLabel: UILabel!
+    @IBOutlet weak var atlDateValueLabel: UILabel!
+    
+    // MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,3 +44,4 @@ class DetailsView: UIViewController {
     }
 
 }
+
