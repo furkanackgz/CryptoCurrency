38f9e8b92974a396915e99781b5edbaa94c4720a
furkanackgz <furkanackgz99@gmail.com>
2022-09-28 03:09:39 +0300

    Add view layer functionality and contract file, import charts library
    And refactor some features in view layer in details module

--- CryptoCurrency/Details Module/Contract/DetailsContract.swift
+++ CryptoCurrency/Details Module/Contract/DetailsContract.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+import Charts
+
+// MARK: - DetailsContract
+struct DetailsContract {
+    typealias detailsView = DetailsViewProtocol
+    typealias detailsInteractor = DetailsInteractorProtocol
+    typealias detailsPresenter = DetailsPresenterProtocol
+    typealias detailsRouter = DetailsRouterProtocol
+}
+
+// MARK: - DetailsViewProtocol
+protocol DetailsViewProtocol: AnyObject {
+    // Presenter related
+    var detailsPresenter: DetailsContract.detailsPresenter? { get set }
+    func setupUI(selectedCurrency: Currency,
+                 isCurrencySaved: Bool,
+                 chartData: LineChartData)
+}
+
+// MARK: - DetailsInteractorProtocol
+protocol DetailsInteractorProtocol: AnyObject {
+    // Presenter related
+    var detailsPresenter: DetailsContract.detailsPresenter? { get set }
+    var selectedCurrency: Currency? { get set }
+    func addToPortfolio()
+    func removeFromPortfolio()
+}
+
+// MARK: - DetailsPresenterProtocol
+protocol DetailsPresenterProtocol: AnyObject {
+    // View related
+    var detailsView: DetailsContract.detailsView! { get set }
+    func viewDidLoad()
+    func didPressBarButton(of type: String)
+    
+    // Interactor related
+    var detailsInteractor: DetailsContract.detailsInteractor! { get set }
+    func didFinishBarButtonAction()
+    
+    // Router related
+    var detailsRouter: DetailsContract.detailsRouter! { get set }
+}
+
+// MARK: - DetailsRouterProtocol
+protocol DetailsRouterProtocol: AnyObject {
+    // Presenter related
+    var detailsView: DetailsView? { get set }
+    func navigateBackToPreviousPage()
+}
--- CryptoCurrency/Details Module/View/DetailsView.swift
+++ CryptoCurrency/Details Module/View/DetailsView.swift
@@ -6,15 +6,16 @@
 //
 
 import UIKit
+import Charts
+import Kingfisher
 
-class DetailsView: UIViewController {
-
+class DetailsView: UIViewController, DetailsContract.detailsView {
     // MARK: - Features
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var currencyImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var symbolLabel: UILabel!
-    @IBOutlet weak var chartView: UIView!
+    @IBOutlet weak var chartView: LineChartView!
     @IBOutlet weak var currentPriceValueLabel: UILabel!
     @IBOutlet weak var marketCapValueLabel: UILabel!
     @IBOutlet weak var marketCapRankValueLabel: UILabel!
@@ -36,12 +37,219 @@ class DetailsView: UIViewController {
     @IBOutlet weak var atlChangePercentageValueLabel: UILabel!
     @IBOutlet weak var atlDateValueLabel: UILabel!
     
+    var detailsPresenter: DetailsContract.detailsPresenter?
+    
+    private var isCurrencySaved: Bool?
+    
     // MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        detailsPresenter?.viewDidLoad()
     }
 
 }
 
+// Presenter related
+extension DetailsView {
+    
+    // MARK: - setupUI
+    func setupUI(selectedCurrency: Currency,
+                 isCurrencySaved: Bool,
+                 chartData: LineChartData) {
+        
+        // Set background color
+        setBackgroundColor()
+        
+        // Set is currency saved variable
+        self.isCurrencySaved = isCurrencySaved
+        
+        // Set navigation bar
+        setNavigationBar(id: selectedCurrency.id!)
+        
+        // Set image view
+        let imageSource = selectedCurrency.image!
+        currencyImageView.kf.setImage(with: URL(string: imageSource))
+        
+        // Set chart view
+        setChartView(with: chartData)
+        
+        // Set labels
+        setLabels(with: selectedCurrency)
+        
+    }
+    
+    /**
+     Sets background color as gradient.
+     */
+    // MARK: - setBackgroundColor
+    private func setBackgroundColor() {
+        
+        let colorTop = ThemeManager.colors.backgroundColor!
+        let colorBottom = UIColor.black
+        
+        let gl = CAGradientLayer()
+        
+        gl.frame = view.bounds
+        gl.colors = [colorTop.cgColor, colorBottom.cgColor]
+        view.layer.insertSublayer(gl, at: 0)
+        containerView.backgroundColor = .clear
+        
+    }
+    
+    // MARK: - setNavigationBar
+    private func setNavigationBar(id: String) {
+        
+        // Set navigation bar back button
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.topItem?.backButtonDisplayMode = .minimal
+        
+        // Create navigation title's attributes
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.init(name: "Marker Felt", size: 26)!,
+            .foregroundColor: UIColor.white
+        ]
+        
+        // Create navigation bar's appearance
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .clear
+        appearance.titleTextAttributes = attributes
+        
+        // Set navigation bar appearance
+        navigationController?.navigationBar.standardAppearance = appearance
+        
+        // Set navigation bar title
+        
+        title = "\(id)".uppercased()
+        
+        // Set navigation bar button
+        if isCurrencySaved! {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Remove",
+                                                               style: .done,
+                                                               target: self,
+                                                               action: #selector(didPressBarButton))
+        } else {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add",
+                                                               style: .done,
+                                                               target: self,
+                                                               action: #selector(didPressBarButton))
+        }
+        
+    }
+    
+    // MARK: - setChartView
+    private func setChartView(with chartData: LineChartData) {
+        chartView.data = chartData
+        chartView.xAxis.labelTextColor = .clear
+        chartView.rightAxis.enabled = false
+        chartView.leftYAxisRenderer.axis.labelTextColor = .white
+        chartView.legend.setCustom(entries: [.init(label: "Current Price")])
+        chartView.legend.textColor = .white
+        chartView.legend.drawInside = true
+        chartView.legend.horizontalAlignment = .left
+        chartView.legend.verticalAlignment = .top
+        chartView.legend.font = UIFont.init(name: "Marker Felt", size: 16)!
+    }
+    
+    // MARK: - setLabels
+    private func setLabels(with selectedCurrency: Currency) {
+        // Set name label
+        nameLabel.text = selectedCurrency.name ?? "-"
+        
+        // Set symbol label
+        symbolLabel.text = selectedCurrency.symbol ?? "-"
+        
+        // Set current price label
+        currentPriceValueLabel.text = String(selectedCurrency.currentPrice ?? 0)
+        
+        // Set market cap label
+        marketCapValueLabel.text = String(selectedCurrency.marketCap ?? 0)
+        
+        // Set market cap rank label
+        marketCapRankValueLabel.text = String(selectedCurrency.marketCapRank ?? 0)
+        
+        // Set fully diluted valuation label
+        fullyDilutedValuationValueLabel.text = String(selectedCurrency.fullyDilutedValuation ?? 0)
+        
+        // Set total volume label
+        totalVolumeValueLabel.text = String(selectedCurrency.totalVolume ?? 0)
+        
+        // Set high 24h label
+        high24hValueLabel.text = String(selectedCurrency.high24h ?? 0)
+        
+        // Set low 24h label
+        low24hValueLabel.text = String(selectedCurrency.low24h ?? 0)
+        
+        // Set price change 24h label
+        priceChange24hValueLabel.text = String(selectedCurrency.priceChange24h ?? 0)
+        
+        // Set price change percentage 24h label
+        priceChangePercentage24hValueLabel.text = String(selectedCurrency.priceChangePercentage24h ?? 0)
+        
+        // Set market cap change 24h label
+        marketCapChange24hValueLabel.text = String(selectedCurrency.marketCapChange24h ?? 0)
+        
+        // Set marketCapChangePercentage24h
+        marketCapChangePercentage24hValueLabel.text = String(selectedCurrency.marketCapChangePercentage24h ?? 0)
+        
+        // Set circulating supply label
+        circulatingSupplyValueLabel.text = String(selectedCurrency.circulatingSupply ?? 0)
+        
+        // Set total supply label
+        totalSupplyValueLabel.text = String(selectedCurrency.totalSupply ?? 0)
+        
+        // Set max supply label
+        maxSupplyValueLabel.text = String(selectedCurrency.maxSupply ?? 0)
+        
+        // Set ath label
+        athValueLabel.text = String(selectedCurrency.ath ?? 0)
+        
+        // Set ath change percentage label
+        athChangePercentageValueLabel.text = String(selectedCurrency.athChangePercentage ?? 0)
+        
+        // Set ath date label
+        let athDate = arrangeDateLabels(date: selectedCurrency.athDate ?? "-")
+        athDateValueLabel.text = athDate
+        
+        // Set atl label
+        atlValueLabel.text = String(selectedCurrency.atl ?? 0)
+        
+        // Set atl change percentage
+        atlChangePercentageValueLabel.text = String(selectedCurrency.atlChangePercentage ?? 0)
+        
+        // Set atl date label
+        let atlDate = arrangeDateLabels(date: selectedCurrency.atlDate ?? "-")
+        atlDateValueLabel.text = atlDate
+    }
+    
+    // MARK: - arrangeDateLabels
+    private func arrangeDateLabels(date: String) -> String {
+        
+        var arrangedDate = date
+        
+        // Get the index of first 10 letters
+        let versionIndex = arrangedDate.index(arrangedDate.startIndex, offsetBy: 10)
+        
+        // Remove the letters of first 10 index
+        arrangedDate.removeSubrange(versionIndex...)
+        
+        return arrangedDate
+        
+    }
+    
+}
+
+// @objc methods
+extension DetailsView {
+    
+    // MARK: - didPressBarButton
+    @objc private func didPressBarButton() {
+        if isCurrencySaved! {
+            detailsPresenter?.didPressBarButton(of: "Remove")
+        } else {
+            detailsPresenter?.didPressBarButton(of: "Add")
+        }
+    }
+    
+}
